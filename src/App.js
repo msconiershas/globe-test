9eed82869f737f4f909cdacf76c2e2af6baee547
msconiershas <41244371+msconiershas@users.noreply.github.com>
2018-09-02 18:47:53 -0500

    Changed colors

--- src/App.js
+++ src/App.js
@@ -1,145 +1,94 @@
-import React, { Component } from 'react';
-import './App.css';
-import fire from './fire';
-import Heading from './Heading'
-import EarthProjection from './components/EarthProjections'
+import React, { Component } from "react"
+import { render } from "react-dom"
+import { Grid, Row, Col } from 'react-material-responsive-grid';
+
+import Map from "./components/Map"
+
+import fire from './fire'
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+
+import * as routes from './assets/routes';
+import "./styles.css"
+
 
 class App extends Component {
-   constructor(props) {
-    super(props);
-    //this.state = { messages: [] }; // <- set up react state
-    this.state = {matches: [], name: '', city: ''};
+  constructor() {
+    super()
+    this.state = { 
+      center: [0, 0], 
+      authUser: null
+    }
 
-
-    this.formPreventDefault = this.formPreventDefault.bind(this);
-    this.onClickPreventDefault = this.onClickPreventDefault.bind(this);
-    this.getItems = this.getItems.bind(this)
-    this.onSubmitForm = this.onSubmitForm.bind(this)
-    this.onCityChange = this.onCityChange.bind(this)
-    this.onNameChange = this.onNameChange.bind(this)
-    this.clearFields = this.clearFields.bind(this)
+    this.authListener = this.authListener.bind(this);
   }
-  
-  // handler recieves the `e` event object
-  formPreventDefault(e) {
-    alert('here');  
-    e.preventDefault();
-  }
-  
-  onClickPreventDefault(e) {
-    alert('onClickPreventDefault called, form will not submit');
-    e.preventDefault();
-  }
- 
- 
-  componentWillMount(){
-
-    this.getItems()
-
-   
-    
-    var i;
-    var j;
-    
+  changeCenter = center => () => {
+    this.setState({ center })
   }
 
-  onSubmitForm(e) {
-    e.preventDefault();
-    console.log(this.state)
-    fire.firestore()
-    .collection('cafes')
-    .doc(this.state.name)
-    .set({
-        name: this.state.name,
-        city: this.state.city
-    })
-    this.state.name = ''
-    this.clearFields();
-  } 
-
-  handleDelete(index) {
-
-    var matches = this.state.matches
-    console.log(matches)
-    var i = matches.splice(index, 1)
-    var id = i[0].id
-    
-    fire.firestore().collection('cafes').doc(id).delete()
-    
-    
-    this.setState({matches})
-    console.log(this.state.matches)
-  }
-
-  onNameChange(e) {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  onCityChange(e) {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  clearFields() {
-    this.setState({name: '', city: ''})
+  componentDidMount() {
+    fire.auth().onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+    console.log(this.state.authUser)
   }
 
 
-  getItems() {
-    const matches = [];
-      fire.firestore().collection('cafes')
-      .orderBy('city').onSnapshot(snapshot => {
-        let changes = snapshot.docChanges();
-        changes.forEach((change, index) => {
-          if(change.type == 'added') {
+  componentWillMount() {
+    console.log("counteedssdsd")
+    this.authListener();
+  }
 
-            matches.push({
-            name: change.doc.data().name,
-            city: change.doc.data().city,
-            id: change.doc.id
-          })
-          }
-          
+  authListener() {
+    this.fireBaseListener = fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+
+        console.log('user changed..', user);
+         this.setState({
+            loading: false,  // For the loader maybe
+            user, // User Details
+            isAuth: true
+      });
+      } else {
+        console.log('user does not exist')
+        console.log(this.props.isEmpty)
+        this.setState({
+          user
         })
-        this.setState({matches})
-      })
-     
       }
+    });
+    }
 
+  componentWillUnmount() {
+     this.fireBaseListener && this.fireBaseListener();
+     this.authListener = undefined;
+  }
+  onLogout(e) {
+      fire.auth().signOut();
+      
+    }
   render() {
-    console.log(this.props)
-
     return (
       <div>
-      <div className="content">
-        
+
+        <div>
+         
+            
+         <Map />
           
-        <form   onSubmit={this.onSubmitForm.bind(this)}   id="add-cafe-form">
-               <input type="text" value={this.state.name} name="name" placeholder="Cafe name" onChange={this.onNameChange}></input>
-                <input type="text" value={this.state.city} name="city" placeholder="Cafe city" onChange={this.onCityChange}></input>
-                <button  >Add Cafe</button>
-        </form>
-
-         <ul id='cafe-list'>
-                {this.state.matches.map((v,index) => {
-          return (
-            
-             <li key={index} data-id={v.id}> <span> {v.name}</span> <span>{v.city}</span> <div onClick={this.handleDelete.bind(this, index)} >x</div> </li>
-            
-          );
-        })}
-        </ul>
-        <Heading />
-        <EarthProjection />
-
-          <div >
+          </div>
+       
       </div>
-        
-        
-      </div>
-        
-      </div>
-    );
+    )
   }
 }
+
+const mapStateToProps = state => ({
+  user: state.firebase.profile,
+  isEmpty: state.firebase.profile.isEmpty
+})
 
 export default App;
